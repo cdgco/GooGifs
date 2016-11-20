b444d792762b7dfb2f8f6df0065f89d741fc0f5b
CDG <cdgco@users.noreply.github.com>
2016-11-20 13:42:40 -0800

    Update search-api.php

--- search-api.php
+++ search-api.php
@@ -2,14 +2,33 @@
 
 include 'config.php';
 
+$gifnum = $_GET['gifnumber'];
+$startnumber = '0';
+
 $s0 = "http://api.giphy.com/v1/gifs/search?api_key=$apikey&limit=100&rating=pg-13";
 $s1 = $s0 . '&q=' . $_GET['gifquery'];
 $s2 = file_get_contents($s1);
 $s3 = json_decode($s2, true);
 $s4 = $s3[data];
-$s5 = $s4[$_GET['gifnumber']];
+
+if ($gifnum == 1) {
+
+} 
+elseif ($gifnum == 2) {
+    echo "'{$element}' is NOT numeric", PHP_EOL;
+};
+
+if (is_numeric($startnumber)) {
+    $mainnumber = gmp_add($startnumber, $gifnum); ;
+} else {
+    $mainnumber = 0;
+  
+};
+
+$s5 = $s4[$mainnumber];
 $s6 = $s5[images];
 $s7 = $s6[original];
 $search = $s7[url];
 
+print_r($_GET['element']);
 ?>
