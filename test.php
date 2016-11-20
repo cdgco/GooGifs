4f123d14f58ca545ba9f33d9221df31574020de6
Carter Roeser <carter@cdgtech.one>
2016-11-19 16:49:18 -0800

    Security Remider for Config Manager

--- test.php
+++ test.php
@@ -173,7 +173,14 @@
 		Your Support email is set as: <?php include 'config.php'; print_r($email) ?>
 		<br>
 		<br><hr style="color:black"><br><br>
-		To change these values, <a href="install">re-install the configuration</a>.<br><br><p>It is recommended that you delete the "install" directory after you have configured these values.</p><br><a href="index.php">Go to Home</a>
+		To change these values, <a href="install">re-install the configuration</a>. You may now go to the <a href="/">Home Page</a>.
+<br><br><br>
+
+<?php $filename = 'install/install.php';
+
+if (file_exists($filename)) { echo '<p><b style="font-size:20px; color:red">WARNING: The "Install" Directory Still Exists, It is recommended that you delete this if possible. </b><a style="font-size:20px;" href="test.php">Re-Check</a></p>';} 
+else { echo '<p><b style="font-size:15px; color:green">Install Directory has been successfully removed!</b></p>';}
+?>
 	    <script>
       function ErrorPage(e,t,n){this.$container=$(e),this.$contentContainer=this.$container.find(n=="sign"?".sign-container":".content-container"),this.pageType=t,this.templateName=n}ErrorPage.prototype.centerContent=function(){var e=this.$container.outerHeight(),t=this.$contentContainer.outerHeight(),n=(e-t)/2,r=this.templateName=="sign"?-100:0;this.$contentContainer.css("top",n+r)},ErrorPage.prototype.initialize=function(){var e=this;this.centerContent(),this.$container.on("resize",function(t){t.preventDefault(),t.stopPropagation(),e.centerContent()}),this.templateName=="plain"&&window.setTimeout(function(){e.$contentContainer.addClass("in")},500),this.templateName=="sign"&&$(".sign-container").animate({textIndent:0},{step:function(e){$(this).css({transform:"rotate("+e+"deg)","transform-origin":"top center"})},duration:1e3,easing:"easeOutBounce"})},ErrorPage.prototype.createTimeRangeTag=function(e,t){return"<time utime="+e+' simple_format="MMM DD, YYYY HH:mm">'+e+"</time> - <time utime="+t+' simple_format="MMM DD, YYYY HH:mm">'+t+"</time>."},ErrorPage.prototype.handleStatusFetchSuccess=function(e,t){if(e=="503")$("#replace-with-fetched-data").html(t.status.description);else if(!t.scheduled_maintenances.length)$("#replace-with-fetched-data").html("<em>(there are no active scheduled maintenances)</em>");else{var n=t.scheduled_maintenances[0];$("#replace-with-fetched-data").html(this.createTimeRangeTag(n.scheduled_for,n.scheduled_until)),$.fn.localizeTime()}},ErrorPage.prototype.handleStatusFetchFail=function(e){$("#replace-with-fetched-data").html("<em>(enter a valid StatusPage.io url)</em>")},ErrorPage.prototype.fetchStatus=function(e,t){if(!e||!t||t=="404")return;var n="",r=this;t=="503"?n=e+"/api/v2/status.json":n=e+"/api/v2/scheduled-maintenances/active.json",$.ajax({type:"GET",url:n}).success(function(e,n){r.handleStatusFetchSuccess(t,e)}).fail(function(e,n){r.handleStatusFetchFail(t)})};
       var ep = new ErrorPage('body', "404", "plain");
